dc79d565ceb6ca7bfc1092bf20857d836382ce07
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-05-16 09:08:37 -0300

    a

--- tela-principal/tela-principal.js
+++ tela-principal/tela-principal.js
@@ -1,4 +1,7 @@
-
+function replaceName(image)
+{
+    namejs.src=image;
+}
 function replaceCart(image)
 {
     cart.src=image;
@@ -15,36 +18,6 @@ function replaceAffiliate(image)
 {
     affiliate.src=image;
 }
-
-function validateCEP() 
-{
-    expressao = /^[0-9]{5}-[0-9]{3}$/g;
-    texto = cep.value
-    resposta = expressao.test(texto);
-    if (resposta == false) 
-    {
-        cep.classList.remove('certo');
-        cep.classList.add('errado');
-    }
-    if (resposta == true) 
-    {
-        cep.classList.remove('errado');
-        cep.classList.add('certo');
-    }
-}
-function calculateShipping() 
-{
-    expressao = /^[0-9]{5}-[0-9]{3}$/g;
-    texto = cep.value
-    resposta = expressao.test(texto);
-    if (resposta == true) 
-    {
-        const cep = document.getElementById("cep").value;
-        const shipping = Math.random() * 50; // valor de frete fictício
-        const freteFormatado = shipping.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        document.getElementById("frete").textContent = `O valor do frete para o CEP: ${cep} é de ${freteFormatado}.`;
-    }
-}  
 
 
 
